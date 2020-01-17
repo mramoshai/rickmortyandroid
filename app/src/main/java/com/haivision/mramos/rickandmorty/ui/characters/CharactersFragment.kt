db65871326201eaa8693a498ee6157d292ee4d6e
Manuel Ramos <mramos@haivision.com>
2020-01-17 12:10:38 +0100

    button to load characters

--- app/src/main/java/com/haivision/mramos/rickandmorty/ui/characters/CharactersFragment.kt
+++ app/src/main/java/com/haivision/mramos/rickandmorty/ui/characters/CharactersFragment.kt
@@ -4,7 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -22,9 +23,11 @@ class CharactersFragment : Fragment() {
         viewModel =
                 ViewModelProviders.of(this).get(CharactersViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_characters, container, false)
-        val textView: TextView = root.findViewById(R.id.text_characters)
+        val button: Button = root.findViewById(R.id.load_characters_button)
+        button.setOnClickListener { viewModel.loadCharacters() }
         viewModel.characters.observe(viewLifecycleOwner, Observer {
-            textView.text = it.joinToString { it.name }
+            val names = it.joinToString { it.name }
+            Toast.makeText(context, names, Toast.LENGTH_SHORT ).show()
         })
         return root
     }
--- app/src/main/java/com/haivision/mramos/rickandmorty/ui/characters/CharactersViewModel.kt
+++ app/src/main/java/com/haivision/mramos/rickandmorty/ui/characters/CharactersViewModel.kt
@@ -3,15 +3,15 @@ package com.haivision.mramos.rickandmorty.ui.characters
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class CharactersViewModel : ViewModel() {
 
-    private val _characters = MutableLiveData<List<Character>>().apply {
-        value = getCharacters()
-    }
+    private val _characters = MutableLiveData<List<Character>>()
     val characters: LiveData<List<Character>> = _characters
 
-    private fun getCharacters(): List<Character> {
-        return STATIC_CHARACTERS
+    fun loadCharacters() {
+        viewModelScope.launch { _characters.value = STATIC_CHARACTERS }
     }
 }
