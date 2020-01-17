f7ef045f1c7a856263ed703e28a5ee801d917ed8
Manuel Ramos <mramos@haivision.com>
2020-01-17 14:56:50 +0100

    removing button, refactoring

--- app/src/main/java/com/haivision/mramos/rickandmorty/ui/characters/CharactersFragment.kt
+++ app/src/main/java/com/haivision/mramos/rickandmorty/ui/characters/CharactersFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -24,28 +23,25 @@ class CharactersFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        viewModel =
-                ViewModelProviders.of(this).get(CharactersViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(CharactersViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_characters, container, false)
 
+        setUpRecyclerView(root)
+        viewModel.loadCharacters()
+        viewModel.characters.observe(viewLifecycleOwner, Observer(this::processCharacters))
+        return root
+    }
+
+    private fun processCharacters(it: List<Character>) {
+        if (it.isNotEmpty()) recyclerView.adapter = CharacterAdapter(it)
+    }
+
+    private fun setUpRecyclerView(root: View) {
         viewManager = LinearLayoutManager(context)
-        viewAdapter = CharacterAdapter(STATIC_CHARACTERS)
+        viewAdapter = CharacterAdapter(listOf())
         recyclerView = root.findViewById<RecyclerView>(R.id.characters_recycler_view).apply {
             layoutManager = viewManager
             adapter = viewAdapter
         }
-        recyclerView.visibility = View.GONE
-
-        val button: Button = root.findViewById(R.id.load_characters_button)
-        button.setOnClickListener { viewModel.loadCharacters() }
-
-        viewModel.characters.observe(viewLifecycleOwner, Observer {
-            if (it.isNotEmpty()) {
-                recyclerView.adapter = CharacterAdapter(it)
-                recyclerView.visibility = View.VISIBLE
-                button.visibility = View.GONE
-            }
-        })
-        return root
     }
 }
