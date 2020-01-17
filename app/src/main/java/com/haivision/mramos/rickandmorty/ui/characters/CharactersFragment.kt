d7e01234dc97dfbf19adc1e3edcfb4e08c3f606d
Manuel Ramos <mramos@haivision.com>
2020-01-17 14:46:31 +0100

    button loads characters

--- app/src/main/java/com/haivision/mramos/rickandmorty/ui/characters/CharactersFragment.kt
+++ app/src/main/java/com/haivision/mramos/rickandmorty/ui/characters/CharactersFragment.kt
@@ -1,12 +1,10 @@
 package com.haivision.mramos.rickandmorty.ui.characters
 
-import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -36,15 +34,17 @@ class CharactersFragment : Fragment() {
             layoutManager = viewManager
             adapter = viewAdapter
         }
+        recyclerView.visibility = View.GONE
 
         val button: Button = root.findViewById(R.id.load_characters_button)
         button.setOnClickListener { viewModel.loadCharacters() }
-        button.visibility = View.GONE
 
         viewModel.characters.observe(viewLifecycleOwner, Observer {
-            // TODO recycler view
-            val names = it.joinToString { it.name }
-            Toast.makeText(context, names, Toast.LENGTH_SHORT ).show()
+            if (it.isNotEmpty()) {
+                recyclerView.adapter = CharacterAdapter(it)
+                recyclerView.visibility = View.VISIBLE
+                button.visibility = View.GONE
+            }
         })
         return root
     }
